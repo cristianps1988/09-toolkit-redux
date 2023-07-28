f04fb6de3729f7c33c114f213366c775dbd9aea6
Cristianps1988 <cristianps_1@hotmail.com>
2023-07-27 21:19:11 -0500

    useGetTodos hook added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import { PokemonApp } from './PokemonApp.jsx'
+import { TodosApi } from './TodosApi.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodosApi />
     </Provider>
   </React.StrictMode>,
 )
